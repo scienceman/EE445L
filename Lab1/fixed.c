65ca9b828b98d73158d7679b993b356a571a8b25
rodriguezg68 <rodriguezg68@utexas.edu>
2014-01-24 16:52:18 -0600

    added fixed.c with header(todo) and functions that need to be written

---
+++ Lab1/fixed.c
@@ -0,0 +1,19 @@
+/***************************************************
+  File Name: fixed.c
+  Author(s): Kevin Gilbert and Gilberto Rodriguez
+  Initial Creation Date: 1/22/14
+  Description: stuff
+  Lab Number: 1
+  TA: some dude
+  Date of last revision: 1/24/14
+  Hardware Configuration: n/a
+***************************************************/
+#include <stdio.h>
+#include "fixed.h"
+
+void Fixed_uDecOut2s(unsigned long n,  char *string);
+void Fixed_uDecOut2(unsigned long n);
+void Fixed_sDecOut3s(long n, char *string);
+void Fixed_sDecOut3(long n);
+void Fixed_uBinOut8s(unsigned long n,  char *string); 
+void Fixed_uBinOut8(unsigned long n); 
