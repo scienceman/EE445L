3a1bc8e4891bb6daa752f25a20a203b53cdea53a
Kevin Gilbert <K.Gilbert.CH@gmail.com>
2014-02-08 01:29:49 -0600

    Added systick interrupts file to project

--- Lab3/main.c
+++ Lab3/main.c
@@ -4,6 +4,15 @@
 #include "sound.h"
 
 int main(void){
-	return 0;
-	//science
+	// Initializations
+	
+	// 
+	while(1){
+		//while(!update);	 // Wait for systick
+		//reposition in RAM
+		//redraw image on OLED
+	}
+	
 }
+
+
