78500aeb545f815ece65e57150507bea22f13afa
Kevin Gilbert <K.Gilbert.CH@gmail.com>
2014-02-09 04:47:49 -0600

    Updated GPIOG interrupt register to NVIC_PRI7_R

--- Lab3/main.c
+++ Lab3/main.c
@@ -4,6 +4,14 @@
 #include "SysTick.h"
 #include "sound.h"
 #include "Output.h"
+#include "switch.h"
+#include "lm3s1968.h"
+
+void DisableInterrupts(void); // Disable interrupts
+void EnableInterrupts(void);  // Enable interrupts
+long StartCritical (void);    // previous I bit, disable interrupts
+void EndCritical(long sr);    // restore I bit to previous value
+void WaitForInterrupt(void);  // low power mode
 
 // delay function for testing from sysctl.c
 // which delays 3*ulCount cycles
@@ -42,7 +50,8 @@ int main(void){
 	 //RIT128x96x4_Line(62,47,82,67,15);
 	 //Minute hand
 	 RIT128x96x4_Line(64,48,35,40,15);
-	 RIT128x96x4_ShowImage();	  
+	 RIT128x96x4_ShowImage();
+	 EnableInterrupts();	  
 
 
 	// 
@@ -50,8 +59,8 @@ int main(void){
 		//while(!update);	 // Wait for systick
 		//reposition in RAM
 		//redraw image on OLED
-	    if (i == 500){
-           // GPIO_PORTG_DATA_R ^= 0x04; //heartbeat
+	    if (i == 500000){
+            //GPIO_PORTG_DATA_R ^= 0x04; //heartbeat
             i = 0;
         }
         //printf("(1) Set Time\r");
--- Lab3/switch.c
+++ Lab3/switch.c
@@ -1,6 +1,8 @@
 #include "switch.h"
 #include "lm3s1968.h"
 
+//#define NVIC_PRI0_R (*((volatile unsigned long*)0xE000E400)) 
+
 void Switch_Init(void){
     int delay;
     SYSCTL_RCGC2_R |= SYSCTL_RCGC2_GPIOG;
@@ -12,13 +14,15 @@ void Switch_Init(void){
     GPIO_PORTG_IS_R &= 0x0F;
     GPIO_PORTG_IBE_R &= 0x0F;
     GPIO_PORTG_IEV_R &= 0x0F;
-    GPIO_PORTG_ICR_R = 0xF0;
+    GPIO_PORTG_ICR_R |= 0xF0;
     GPIO_PORTG_IM_R |= 0xF0;
     GPIO_PORTG_DATA_R &= 0xFB;        // Clear PG2
-    //NVIC_PRI0_R = (NVIC_PRIO_R & 0x00FFFFFF) | 0x40000000;
+    NVIC_PRI7_R = (NVIC_PRI7_R & 0x00FFFFFF) | 0x40000000;	// Priority bits for PortG 31-29
     NVIC_EN0_R |= (NVIC_EN0_INT2 + NVIC_EN0_INT3);
+	//NVIC_EN0_R |= 0x80000;
 }
 
 void GPIOPortG_Handler(void){
     GPIO_PORTG_ICR_R = 0xF0;
+	GPIO_PORTG_DATA_R |= 0x04;
 }
