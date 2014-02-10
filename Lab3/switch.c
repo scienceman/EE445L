b8c5a62a6e70ea10daf903ef422df01d2a823a6d
rodriguezg68 <rodriguezg68@utexas.edu>
2014-02-09 19:06:54 -0600

    the onboard pins should work (not select) and maybe interrput wil fire

--- Lab3/switch.c
+++ Lab3/switch.c
@@ -7,17 +7,17 @@ void Switch_Init(void){
     int delay;
     SYSCTL_RCGC2_R |= SYSCTL_RCGC2_GPIOG;
     delay = SYSCTL_RCGC2_R;
-    GPIO_PORTG_DIR_R &= 0x0F;  // enable PG7-4
+    GPIO_PORTG_DIR_R &= ~0x78;  // enable PG7-4
     GPIO_PORTG_DIR_R |= 0x04;  // set up PG2 LED
-    GPIO_PORTG_DEN_R |= 0xF4;
-    GPIO_PORTG_PUR_R |= 0xF0;  
-    GPIO_PORTG_IS_R &= 0x0F;
-    GPIO_PORTG_IBE_R &= 0x0F;
-    GPIO_PORTG_IEV_R &= 0x0F;
-    GPIO_PORTG_ICR_R |= 0xF0;
-    GPIO_PORTG_IM_R |= 0xF0;
+    GPIO_PORTG_DEN_R |= 0x7C;
+    GPIO_PORTG_PUR_R |= 0x78;  
+    GPIO_PORTG_IS_R &= ~0x78;
+    GPIO_PORTG_IBE_R &= ~0x78;
+    GPIO_PORTG_IEV_R &= ~0x78;
+    GPIO_PORTG_ICR_R |= 0x78;
+    GPIO_PORTG_IM_R |= 0x78;
     GPIO_PORTG_DATA_R &= 0xFB;        // Clear PG2
-    NVIC_PRI7_R = (NVIC_PRI7_R & 0x00FFFFFF) | 0x40000000;	// Priority bits for PortG 31-29
+    NVIC_PRI0_R = (NVIC_PRI0_R & 0x00FFFFFF) | 0x40000000;	// Priority bits for PortG 31-29
     NVIC_EN0_R |= (NVIC_EN0_INT2 + NVIC_EN0_INT3);
 	//NVIC_EN0_R |= 0x80000;
 }
@@ -25,6 +25,6 @@ void Switch_Init(void){
 
 
 void GPIOPortG_Handler(void){
-    GPIO_PORTG_ICR_R = 0xF0;
+    GPIO_PORTG_ICR_R = 0x78;
 	GPIO_PORTG_DATA_R |= 0x04;
 }
