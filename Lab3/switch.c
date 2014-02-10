3f367e3ae993947b078491ec5007ce5f2ab69f7b
Kevin Gilbert <K.Gilbert.CH@gmail.com>
2014-02-10 16:25:48 -0600

    Cleaned up a few lines

--- Lab3/switch.c
+++ Lab3/switch.c
@@ -26,20 +26,7 @@ void Switch_Init(void){
 
 	GPIOPinTypeGPIOInput(GPIO_PORTG_BASE, GPIO_PIN_7);
 	GPIOPinTypeGPIOOutput(GPIO_PORTG_BASE, GPIO_PIN_2);
-
-//    GPIO_PORTG_DIR_R &= ~0x78;  // enable PG7-4
-//    GPIO_PORTG_DIR_R |= 0x04;  // set up PG2 LED
-//    GPIO_PORTG_DEN_R |= 0x7C;
-//    GPIO_PORTG_PUR_R |= 0x78;  
-//    GPIO_PORTG_IS_R &= ~0x78;
-//    GPIO_PORTG_IBE_R &= ~0x78;
-//    GPIO_PORTG_IEV_R |= 0x78;
-//    GPIO_PORTG_ICR_R |= 0x78;
-//    GPIO_PORTG_IM_R |= 0x78;
     GPIO_PORTG_DATA_R &= 0xFB;        // Clear PG2
-//    NVIC_PRI7_R = (NVIC_PRI7_R & 0x00FFFFFF) | 0x40000000;	// Priority bits for PortG 31-29
-//    NVIC_EN0_R |= (NVIC_EN0_INT2 + NVIC_EN0_INT3);
-	//NVIC_EN0_R |= 0x80000;
 	EdgeCounter_Init();
 }
 
@@ -136,9 +123,4 @@ void GPIOPortC_Handler(void) {
 
 void toggleDisplay(void) {
 	displayClock ^= 1;
-}
-
-void GPIOPortG_Handler(void){
-    GPIO_PORTG_ICR_R = 0x78;
-    GPIO_PORTG_DATA_R ^= 0x04;
 }
