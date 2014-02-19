fe603baefdf41c42be94f05ab5d36672139d7c7f
rodriguezg68 <rodriguezg68@utexas.edu>
2014-02-19 13:53:08 -0600

    added music struct and public functions play, rewind, and stop

--- Lab5_SPI-DAC/music.h
+++ Lab5_SPI-DAC/music.h
@@ -4,3 +4,15 @@
  *	Kevin Gilbert, Gilberto Rodriguez
  * 19 February 2014
  ******************************************************/
+
+ struct Musicstruct{
+    char* begin;
+    char* current;
+    int length;
+ };
+
+ typedef struct Music_struct Music;
+
+ void Rewind(void);
+ void Play(char*);
+ void Stop(void);
