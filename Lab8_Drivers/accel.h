f72d472929f4c876da9a752a9556e2d9f729fd5d
unknown <gr7547@ENS252A-02.austin.utexas.edu>
2014-03-26 17:53:41 -0500

    prelab changes

---
+++ Lab8_Drivers/accel.h
@@ -0,0 +1,1 @@
+int* Accel_Recieve(void);
---
+++ Lab8_Drivers/main_drivers.c
@@ -0,0 +1,11 @@
+#include "motor.h"
+#include "sonar.h"
+#include "accel.h"
+#include "Xbee.h"
+#include "system.h"
+
+int main(void) {
+	
+	while(1);
+	return 0;
+}
