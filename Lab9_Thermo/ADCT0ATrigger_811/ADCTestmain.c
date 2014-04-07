afd7dbac1c55c40dae1a8e641ac1cad63b37471c
Kevin Gilbert <K.Gilbert.CH@gmail.com>
2014-04-07 02:44:09 -0500

    Preliminary graphing function added. Untested

--- Lab9_Thermo/ADCT0ATrigger_811/ADCTestmain.c
+++ Lab9_Thermo/ADCT0ATrigger_811/ADCTestmain.c
@@ -33,20 +33,17 @@ long StartCritical (void);    // previous I bit, disable interrupts
 void EndCritical(long sr);    // restore I bit to previous value
 void WaitForInterrupt(void);  // low power mode
 
+unsigned long LinRegADCtoTemp(unsigned long adc);
+void LUTADCtoTemp(void);
+
 #include "lm3s1968.h"
 #include "Output.h"
+#include "Timer0A.h"
+#include "rit128x96x4.h"
+#include "line.h"
 #include <stdio.h>
 
-//debug code
-//
-// This program periodically samples ADC channel 0 and stores the
-// result to a global variable that can be accessed with the JTAG
-// debugger and viewed with the variable watch feature.
-//#define GPIO_PORTC_DATA_R       (*((volatile unsigned long *)0x400063FC))
-//#define GPIO_PORTC_DIR_R        (*((volatile unsigned long *)0x40006400))
-//#define GPIO_PORTC_DEN_R        (*((volatile unsigned long *)0x4000651C))
-//#define SYSCTL_RCGC2_R          (*((volatile unsigned long *)0x400FE108))
-//#define SYSCTL_RCGC2_GPIOC      0x00000004  // port C Clock Gating Control
+void drawGraph_handler(void);
 
 extern volatile unsigned long ADCvalue;
 
@@ -62,26 +59,54 @@ unsigned long const Tdata[53]={4000,4000,3960,3920,3880,3840,3800,3760,3720,3680
      2800,2760,2720,2680,2640,2600,2560,2520,2480,2440,
      2400,2360,2320,2280,2240,2200,2160,2120,2080,2040,2000,2000};
 
+unsigned char graph[(128/2)*96/2] = {0};
+
+void LUTADCtoTemp() {
+	signed long i=0;	
+	for(i=0;i<53;i++) {
+	  if(ADCvalue < ADCdata[i]) {
+	    break;
+	  }
+	}
+	Fixed_uDecOut2(Tdata[i]);
+}
+
+unsigned long LinRegADCtoTemp(unsigned long adc) {
+	unsigned long i=0;
+	i=adc*(-2);   //Line of Best Fit: T(X)=-2.077+3929.95
+	i+=3930;	
+	Fixed_uDecOut2(i);
+	return i;
+}
+
+  volatile int x=0;
+  volatile int y=0;
+  volatile unsigned long prevADC;
+
 int main(void){
-  int i=0;
-  int index=0;
   SYSCTL_RCGC2_R |= SYSCTL_RCGC2_GPIOG;    // activate port C
   ADC_InitTimer0ATriggerSeq3(0, 9, 59999); // ADC channel 0, 10 Hz sampling
   GPIO_PORTG_DIR_R |= 0x04;                // make PC5 out (PC5 built-in LED)
   GPIO_PORTG_DEN_R |= 0x04;                // enable digital I/O on PC5 (default setting)
   Output_Init();
   Output_Color(15);
+
+  Timer0A_Init(drawGraph_handler, 59999);
+
+  RIT128x96x4ImageDraw(graph,0,96/2,128,96/2);   
   while(1){
     WaitForInterrupt();
     GPIO_PORTG_DATA_R ^= 0x04;           // toggle LED
-//	for(i=0;i<53;i++) {
-//	  if(ADCvalue < ADCdata[i]) {
-//	    index = i;
-//	    break;
-//	  }
-//	}
-	i=ADCvalue*(-2);   //Line of Best Fit: T(X)=-2.077+3929.95
-	i+=3930;	
-	Fixed_uDecOut2(i);
+	LinRegADCtoTemp(ADCvalue);
   }
+}
+
+void drawGraph_handler(void) {
+	static int delay=0;
+	if(delay == 0) {
+		RIT128x96x4_Line(x,(int)(LinRegADCtoTemp(prevADC)/100),++x,(int)(LinRegADCtoTemp(ADCvalue)/100),15);
+		RIT128x96x4_ShowImageLines();		
+	} 
+	delay = (delay+1)%10;
+	prevADC = ADCvalue;
 }
---
+++ Lab9_Thermo/line.c
@@ -0,0 +1,88 @@
+#include "line.h"
+#include "rit128x96x4.h"
+#include "math.h"
+#include "stdlib.h"
+
+//*************OLED Buffer*************************************************
+// 4-bit color pixels, each char 2 pixels. Width = 128/2
+//extern unsigned char imageBuff[(128/2)*96];
+//extern unsigned char clock1[];
+
+extern unsigned char graph[(128/2)*96/2];
+unsigned char lineBuff[(128/2)*96];
+
+void mergeBuffers(unsigned char* buff1, unsigned char* buff2, unsigned char* result);
+
+void RIT128x96x4_ShowImage(void) {
+	//mergeBuffers(clock1,graph,imageBuff);
+    //RIT128x96x4ImageDraw(imageBuff,0,0,128,96);
+}
+
+void RIT128x96x4_ShowImageLines(void) {
+	RIT128x96x4ImageDraw(graph,0,96/2,128,96/2);
+}
+
+void RIT128x96x4_Line(int x1, int y1, int x2, int y2, unsigned char color) {
+	int dx,dy,index,sx,sy,err,e2;
+	x1 /= 2;
+	x2 /= 2;
+	dx = abs(x2 - x1);
+	dy = abs(y2 - y1);
+	index = x1+y1*(128/2);
+	if(x1 < x2) {
+		sx = 1;	 	
+	} else {
+	 	sx = -1;
+	}
+	if(y1 < y2) {
+	 	sy = 1;
+	} else {
+	 	sy = -1;
+	}
+	err = dx-dy;
+
+	while(1) {
+	 	graph[index] = color;
+		if(x1 == x2 && y1==y2){
+			return;
+		}
+		e2=err*2;
+		if(e2 > -dy) {
+			err -= dy;
+			x1 += sx;
+		}
+		if(x1 == x2 && y1 == y2) {
+			graph[x1+y1*(128/2)] = color;
+			return;
+		}
+		if(e2 < dx) {
+			err += dx;
+			y1 += sy;
+		}
+		graph[x1+y1*(128/2)] = color;
+	}				 	
+}
+
+void mergeBuffers(unsigned char* buff1,unsigned char* buff2,unsigned char* result) {
+    int i=0;
+	char temp;
+	while(i<(128/2)*96) {
+		temp =(buff1[i]+buff2[i]);
+		if(temp > 0xFF) {
+		 	result[i]=0xFF;
+		} else {
+		 	result[i]=temp;
+		}
+		i++;
+	}
+	return;
+}
+
+void RIT128x96x4_ClearImage(void) {
+	int i,size;
+	size = (128/2)*96;
+	for(i=0;i<size;i++) {
+	    graph[i]=0;
+	}
+	//RIT128x96x4ImageDraw(graph,0,0,128,96);
+}
